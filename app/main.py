2e6b02e05c7793a71fe5517b5ba4bb4b6027189f
Andrew <elbroandrew@gmail.com>
2024-07-22 10:41:52 +1000

    add vue frontend with axios router

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 import fastapi
-import httpx
-from scrapy.selector import Selector
-import time
-import os
+from fastapi.middleware.cors import CORSMiddleware
 
-api = fastapi.FastAPI()
+app = fastapi.FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@api.get('/api/weather/{city}')
-async def weather(city: str):
-    url = f'https://pogoda.mail.ru/prognoz/{city}/'
+@app.get("/")
+def read_root():
+    return "Hello, World!"
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, timeout=None)
-        response.raise_for_status()
-
-    selector = Selector(text=response.text)
-    t = selector.xpath('//div[@class="information__content__temperature"]/text()').getall()[1].strip()
-
-    print("FASTAPI PID: ", os.getpid())
-    time.sleep(6)
-
-    return {'city':city, 'temperature':t, 'source':'pogoda.mail.ru'}
+@app.get("/custom")
+def read_custom_message():
+    return {"message": "This is a custom message!"}
